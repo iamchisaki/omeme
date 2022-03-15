824f31d1ca4895ec0bdd8485b0efbb3a6008e9ef
CHISAKI <99480101+iamchisaki@users.noreply.github.com>
2022-03-15 21:48:59 +0900

    Add files via upload

---
+++ stopWatch.js
@@ -0,0 +1,44 @@
+console.log()//確認方法
+//コメント書ける
+
+const timer = document.getElementById("timer") ; 
+const start = document.getElementById("start") ; 
+const stop = document.getElementById("stop") ;
+const reset = document.getElementById("reset") ;
+//変数の指定
+
+let hrs = 0;
+let min = 0;
+let sec = 0;
+let un = 0;
+
+let Interval;
+
+function stopWatch(){
+un++; //1足す
+if(un / 10 == 1){
+sec++;
+un = 0;
+
+if(sec / 10 == 1){
+min++;
+sec = 0;
+}
+}
+
+timer.innerHTML = hrs + ":" + min + ":" + sec + ":" + un + ":";
+}
+
+start.addEventListener("click",function(){
+ Interval = setInterval(stopWatch)
+})
+
+stop.addEventListener('click',function(){
+clearInterval( Interval);
+})
+
+reset.addEventListener("click", function(){
+ clearInterval(Interval);
+ timer.innerHTML = "0:0:0:0";
+ hrs = 0; min = 0; sec = 0; un = 0;
+})
