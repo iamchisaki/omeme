1b30f1413d0f31be82b5e3612cdb658372ab4790
EC2 Default User <ec2-user@ip-172-31-28-236.ec2.internal>
2022-03-18 15:44:33 +0000

    ボタン機能の編集、秒数の確認

--- stopwatch.js
+++ stopwatch.js
@@ -1,4 +1,4 @@
-console.log()//確認方法
+//console.log()//確認方法
 //コメント書ける
 
 const timer = document.getElementById("timer") ; 
@@ -8,19 +8,17 @@ const reset = document.getElementById("reset") ;
 //変数の指定
 
 
-let hrs = 0;
 let min = 0;
 let sec = 0;
-let un = 0;
+let ms = 0;
 
 let Interval;
 
-
 function stopWatch(){
-un++; //1足す
-if(un / 60 == 1){
+ms++; //1足す
+if(ms / 200 == 1){
 sec++;
-un = 0;
+ms = 0;
 
 if(sec / 60 == 1){
 min++;
@@ -28,21 +26,25 @@ sec = 0;
 }
 }
 
-timer.innerHTML = hrs + ":" + min + ":" + sec + ":" + un + ":";
-}
+
+timer.innerHTML= ('0'+min).substr(-2) + ":" + ('0'+sec).substr(-2) + ":" + ('0'+ms).substr(-2); 
+} 
+
 
 start.addEventListener("click",function(){
  Interval = setInterval(stopWatch)
+this.disabled = true;
 })
 
 stop.addEventListener('click',function(){
 clearInterval( Interval);
+start.disabled = false;
 })
 
 reset.addEventListener("click", function(){
 clearInterval(Interval);
 start.disabled = false;
-timer.innerHTML = "0:0:0:0";
-hrs = 0; min = 0; sec = 0; un = 0;
+timer.innerHTML = "00:00:00";
+//hrs = 0; min = 0; sec = 0; un = 0;
 })
 
